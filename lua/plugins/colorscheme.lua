b71a773ddcad8f20032fb5fbd8933c66cc3f859c
Sumit Achaju <achajusumit@gmail.com>
2025-03-08 10:59:46 +0545

    colorscheme format

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,24 +7,24 @@ return {
       devicons = true, -- highlight the icons of `nvim-web-devicons`
       styles = {
         comment = { italic = true },
-        keyword = { italic = true },       -- any other keyword
-        type = { italic = true },          -- (preferred) int, long, char, etc
-        storageclass = { italic = true },  -- static, register, volatile, etc
-        structure = { italic = true },     -- struct, union, enum, etc
-        parameter = { italic = true },     -- parameter pass in function
+        keyword = { italic = true }, -- any other keyword
+        type = { italic = true }, -- (preferred) int, long, char, etc
+        storageclass = { italic = true }, -- static, register, volatile, etc
+        structure = { italic = true }, -- struct, union, enum, etc
+        parameter = { italic = true }, -- parameter pass in function
         annotation = { italic = true },
         tag_attribute = { italic = true }, -- attribute of tag in reactjs
       },
-      filter = "spectrum",                 -- classic | octagon | pro | machine | ristretto | spectrum
+      filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
       -- Enable this will disable filter option
       day_night = {
-        enable = false,            -- turn off by default
-        day_filter = "pro",        -- classic | octagon | pro | machine | ristretto | spectrum
+        enable = false, -- turn off by default
+        day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
         night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
       },
-      inc_search = "background",   -- underline | background
+      inc_search = "background", -- underline | background
       background_clear = {
-        -- "float_win",
+        "float_win",
         "toggleterm",
         "telescope",
         -- "which-key",
@@ -33,7 +33,7 @@ return {
         "nvim-tree",
         "neo-tree",
         "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-      },              -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+      }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
       plugins = {
         bufferline = {
           underline_selected = false,
@@ -44,13 +44,12 @@ return {
           context_start_underline = false,
         },
       },
-    }
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "monokai-pro",
     },
-  }
-
+  },
 }
